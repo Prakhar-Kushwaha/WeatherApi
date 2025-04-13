735f271f97e74d0a071a50023f58ea8deb1e6b48
prakhar <prakharkushwaha001@gmail.com>
2025-04-13 12:12:52 +0530

    Remove base property for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import {ghPages} from 'vite-plugin-gh-pages'
 
 
 export default defineConfig({
-  plugins: [react() , ghPages()],
-  base: '/Weather-Api/weatherApi/',
+  plugins: [react()],//, ghPages()],
+ // base: '/Weather-Api/weatherApi/',
   server :{
     host : true,
     port : 4578,
